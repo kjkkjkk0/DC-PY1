33616fbc29d32860edc569463e0f5b7033061c0d
kjkkjkk0 <115473947+kjkkjkk0@users.noreply.github.com>
2022-11-14 19:28:48 +0300

    Update task_5.py

--- Лабораторная работа 5/task_5.py
+++ Лабораторная работа 5/task_5.py
@@ -1,12 +1,10 @@
-import random
+from random import sample
 import string
 
 
 def get_random_password(n: int = 8) -> str:
-    return random.sample([lower_letter for lower_letter in string.ascii_lowercase] +
-                         [upper_letter for upper_letter in string.ascii_uppercase] +
-                         [digit for digit in string.digits],
-                         k=n)
+    return ''.join(sample(string.ascii_lowercase + string.ascii_uppercase + string.digits,
+                          k=n))
 
 
-print(''.join(get_random_password()))
+print(get_random_password())
